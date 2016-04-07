082429c8f214afea84f1a0dc9d95b31c8182a4fd
Toomas1968 <toomas.unt1968@gmail.com>
2016-04-07 23:35:22 +0100

    S

---
+++ includes/db.php
@@ -0,0 +1,21 @@
+<?php 
+
+// Connecting to Mysqli database
+/**
+Trying to build more secure method for mysqli connection. Needs more testing in real work environment.
+
+$db['db_host'] = "localhost";
+$db['db_name'] = "cms";
+$db['db_user'] = "root";
+$db['db_pass'] = "";
+
+foreach($db as $key => $value) 
+{
+	define(strtoupper($key), $value);
+}
+ 
+ */
+
+	$mysqli = mysqli_connect('localhost', 'root', '', 'cms');
+
+?>
